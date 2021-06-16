e21f7618dcbe7514f10b479e8266accadc6acd1e
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-06-16 16:35:39 -0400

    backend completed and routes work

---
+++ grocerylist-backend/routes/controller/groceryController.js
@@ -0,0 +1,69 @@
+const Grocery = require("../Model/Grocery");
+
+async function getAllGroceries (req, res) {
+    try {
+        let getAllGroceries = await Grocery.find({})
+        
+        res.json({ message: "success", payload: getAllGroceries })
+    } catch (e) {
+        res.status(500).json({ message: "failure", error: e.message })
+    }
+}
+
+async function createGrocery(req, res){
+    const {grocery, purchased} = req.body
+    
+    let errorObj = {}
+    
+        if(Object.keys(errorObj).length > 0){
+            return res.status(500).json({message:"error", payload: errorObj})
+        }
+        
+        try {
+            let createNewGrocery = new Grocery ({
+                grocery, 
+                purchased,
+                Date: Date.now()
+            })
+
+            let save = await createNewGrocery.save()
+            res.json({message:"Success", save})
+        } catch (error) {
+            res.status(500).json({message:"error", error: error.message})
+        }
+}
+
+async function updateGrocery(req,res){
+    let errorObj = {};
+
+    if(Object.keys(errorObj).length > 0){
+        return res.status(500).json({message:"error", payload: errorObj})
+    }
+
+    try {
+        let grocery = req.body.grocery;
+
+        let updatedGrocery = await Grocery.findByIdAndUpdate(req.params.id, {$set: { grocery: grocery}}, {new:true})
+
+        res.json({message: 'success', payload: updatedGrocery})
+    } catch {
+        res.status(500).json({message:'error'})
+    }
+};
+
+async function deleteGrocery (req, res) {
+    try {
+        console.log('path hit')
+        let delGrocery = await Grocery.findByIdAndRemove(req.params.id)
+        res.json({message:"success", delGrocery})
+    } catch (error) {
+        res.status(500).json({message:'error', error:error.message})
+    }
+};
+
+module.exports = {
+    getAllGroceries,
+    createGrocery,
+    updateGrocery,
+    deleteGrocery, 
+}
---
+++ grocerylist-backend/routes/groceryRouter.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+var {
+    getAllGroceries,
+    createGrocery,
+    updateGrocery,
+    deleteGrocery, 
+} = require("./controller/groceryController");
+
+router.get ('/get-all-groceries', getAllGroceries);
+router.post ('/create-grocery', createGrocery);
+router.put ('/update-grocery-by-id/:id', updateGrocery);
+router.delete ('/delete-grocery-by-id/:id', deleteGrocery);
+
+module.exports = router;
